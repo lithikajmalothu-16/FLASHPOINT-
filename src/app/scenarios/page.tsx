7542c48260ece991f6f81839b6150b1cf1059bea
Lithika J Malothu <lithikajmalothu@gmail.com>
2025-11-09 10:43:28 +0000

    keep letters a little spaced , make upper title itlalics, change nothign

--- src/app/scenarios/page.tsx
+++ src/app/scenarios/page.tsx
@@ -75,7 +75,7 @@ export default function ScenariosPage() {
           <div className="container px-4 md:px-6">
             <div className="flex flex-col items-center justify-center space-y-4 text-center">
               <div className="space-y-2">
-                <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl font-headline uppercase">Train Hard</h2>
+                <h2 className="text-3xl font-bold italic tracking-wider sm:text-5xl font-headline uppercase">Train Hard</h2>
                 <p className="max-w-[900px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
                   RESPOND HARDER
                 </p>
